22b8b2232244a40dc65e9e6f50d9094060320140
Nathan Fishel <nfishel@emich.edu>
2021-02-24 23:15:53 -0500

    added player arrea and pickups

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -30,4 +30,13 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        } 
+        
+    }
 }
